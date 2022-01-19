993dda591dc1b900259839798354c51a6f32ac32
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-01-19 09:19:44 +0100

    date

---
+++ ProductivityTools.Salaries.Api/Controllers/DateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProductivityTools.Salaries.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DateController : ControllerBase
+    {
+        [HttpPost("GetDate")]
+        public string GetDate()
+        {
+            return DateTime.Now.ToString(); 
+        }
+    }
+}
