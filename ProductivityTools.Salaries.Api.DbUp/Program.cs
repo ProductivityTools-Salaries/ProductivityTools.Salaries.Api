95c63dc3aae80babab60b9af7241223f5d4ae340
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-11-05 12:57:12 +0100

    new file format

--- ProductivityTools.Salaries.Api.DbUp/Program.cs
+++ ProductivityTools.Salaries.Api.DbUp/Program.cs
@@ -6,45 +6,36 @@ using System.Linq;
 using System.Reflection;
 
 
-namespace ProductivityTools.Salaries.Api.DbUp
-{
-    class Program
-    {
-        static int Main(string[] args)
-        {
-            IConfigurationRoot configuration =
+IConfigurationRoot configuration =
                 new ConfigurationBuilder()
                 .AddMasterConfiguration(configName: "ProductivityTools.Salaries.Api.json", force: true)
                 .Build();
 
-            var masterConnectionString = configuration["ConnectionString"];
-            var connectionString = args.FirstOrDefault() ?? masterConnectionString;
+var masterConnectionString = configuration["ConnectionString"];
+var connectionString = args.FirstOrDefault() ?? masterConnectionString;
 
-            EnsureDatabase.For.SqlDatabase(connectionString);
-            var upgrader =
-                DeployChanges.To
-                    .SqlDatabase(connectionString)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
+EnsureDatabase.For.SqlDatabase(connectionString);
+var upgrader =
+    DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
 
-            var result = upgrader.PerformUpgrade();
+var result = upgrader.PerformUpgrade();
 
-            if (!result.Successful)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(result.Error);
-                Console.ResetColor();
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
 #if DEBUG
-                Console.ReadLine();
+    Console.ReadLine();
 #endif
-                return -1;
-            }
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Success!");
-            Console.ResetColor();
-            return 0;
-        }
-    }
+    return -1;
 }
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
