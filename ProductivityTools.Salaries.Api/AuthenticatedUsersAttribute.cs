1f83763da4b77e11c511de5a985367883b462cc7
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-11-05 12:27:13 +0100

    echo

--- ProductivityTools.Salaries.Api/AuthenticatedUsersAttribute.cs
+++ ProductivityTools.Salaries.Api/AuthenticatedUsersAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 
@@ -11,18 +12,26 @@ namespace ProductivityTools.Salaries.Api
         }
         public override void OnActionExecuting(Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext context)
         {
-            var x1 = context.HttpContext.User;
-            var identity = (ClaimsIdentity)x1.Identity;
-            IEnumerable<Claim> claims = identity.Claims;
-            var email = claims.First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
-
-            if (email == "pwujczyk@gmail.com")
+            if (((ControllerActionDescriptor)context.ActionDescriptor).ControllerName.ToLower() == "salary" &&
+               ((ControllerActionDescriptor)context.ActionDescriptor).ActionName.ToLower() == "echo")
             {
                 base.OnActionExecuting(context);
             }
             else
             {
-                context.Result = new Microsoft.AspNetCore.Mvc.UnauthorizedResult();
+                var x1 = context.HttpContext.User;
+                var identity = (ClaimsIdentity)x1.Identity;
+                IEnumerable<Claim> claims = identity.Claims;
+                var email = claims.First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+
+                if (email == "pwujczyk@gmail.com")
+                {
+                    base.OnActionExecuting(context);
+                }
+                else
+                {
+                    context.Result = new Microsoft.AspNetCore.Mvc.UnauthorizedResult();
+                }
             }
         }
     }
